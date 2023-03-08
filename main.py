cf7c7cc0adeefcf4e7b37cd00f6f08f59c232357
jpbandroid <100033309+jpbandroid@users.noreply.github.com>
2023-03-08 20:16:49 +0000

    small code updates

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def output_module():
         if module == main_npc:
                         print("You are in the same module as the Main Alien NPC (Telium)")
         if module == vent_shafts[0] or module == vent_shafts[1] or module == vent_shafts[2]:
-                print("You are inside a vent shaft. (sus...)")
+                print("You are inside a vent shaft. (...)")
         if module == info_panels[0] or module == info_panels[1]:
                 print("You are inside a module with an info panel.")
         if module == workers[0] or module == workers[1] or module == workers[2]:
@@ -257,15 +257,15 @@ def move_mainNPC():
                 while main_npc in vent_shafts:
                         #if moves_to_make > 1:
                                 #print("... and has escaped.")
-                        print("We can hear scuttling in the vent shafts...\nI think the alien is sus...")
+                        print("We can hear scuttling in the vent shafts...")
                         valid_move = False
-                        #The Main Alien NPC is unable to land in a module with another (sus) vent shaft...
+                        #The Main Alien NPC is unable to land in a module with another () vent shaft...
                         while valid_move == False:
                                 valid_move = True
                                 main_npc = random.randint(1, num_modules)
                                 if main_npc in vent_shafts:
                                         valid_move = False
-                        #The Main Alien NPC always stops moving after travelling through the (sus) vent shaft
+                        #The Main Alien NPC always stops moving after travelling through the () vent shaft
                         moves_to_make = 0
 
 #Main program starts here
@@ -274,7 +274,7 @@ print('Spaceship Game version 3.0 beta 1\n08/03/2023\nLoad instructions using IN
 
 spawn_npcs()
 print("Main Alien NPC is located in module: " , main_npc)
-print("(sus) vents are located in modules: " , vent_shafts)
+print("Vents are located in modules: " , vent_shafts)
 print("Info panels are located in modules: " , info_panels)
 print("Worker NPC are located in modules:", workers)
 print("Fuel cells are located in modules:", fuel_cells)
